07c038f5ee07fc2bf94239d404cafa5f06204b52
JohnMason1999 <47746539+JohnMason1999@users.noreply.github.com>
2021-11-01 18:43:16 +0000

    added negative mutation drift

--- mutation-bias.py
+++ mutation-bias.py
@@ -6,11 +6,16 @@ import statistics
 
 mutation_rate = 0.05
 vectors = 1
-population_size = 50
+population_size = 25
 scalar_length = 100
 generations = 600
 
-population = poptools.gen_population(population_size,vectors,scalar_length)
+unfit_population = poptools.gen_population(population_size,vectors,scalar_length)
+fit_population = poptools.gen_fittest_population(population_size,vectors,scalar_length)
 for x in range(generations):
-    population = poptools.mutate_population(population,mutation_rate)
-print(statistics.mean(poptools.obj_score_pop(population))) #shows average fitness of the population
+    unfit_population = poptools.mutate_population(unfit_population,mutation_rate)
+    fit_population = poptools.mutate_population(fit_population,mutation_rate)
+
+ #shows average fitness of the populations
+print("unfit population average: "+str((statistics.mean(poptools.obj_score_pop(unfit_population)))))
+print("fit population average: "+str((statistics.mean(poptools.obj_score_pop(fit_population)))))
